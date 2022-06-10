2770b5efdc8553d71fcc93ea18c4fca1941adb64
1n0r1 <khanhn2@illinois.edu>
2022-06-10 14:32:57 +0700

    Create dask version

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import dask.array as da
+
 chunksize = 10**6
 
 fileReader = pd.read_csv('./data/UserAnimeList.csv', chunksize=chunksize)
@@ -9,9 +12,31 @@ count = 0
 
 for chunk in fileReader:
     count +=1
-    if count > 10:
+    if count > 2:
         break
     newdf = chunk.pivot(index='username', columns='anime_id', values='my_score').fillna(0).astype('int8')
     df = pd.concat((df,newdf)).groupby(['username']).first().fillna(0).astype('int8')
     # df.loc[row.iloc[0], row.iloc[1]] = row.iloc[5]=
 print(df)
+
+
+matrix = df.values
+
+u, s, vh = np.linalg.svd(matrix, full_matrices=False)
+# u, s, vh = da.linalg.svd_compressed(matrix, k=10, compute=True)
+ 
+# Find the highest similarity
+def cosine_similarity(v,u):
+    return (v @ u)/ (np.linalg.norm(v) * np.linalg.norm(u))
+ 
+highest_similarity = -np.inf
+highest_sim_col = -1
+for col in range(1,vh.shape[1]):
+    similarity = cosine_similarity(vh[:,0], vh[:,col])
+    if similarity > highest_similarity:
+        highest_similarity = similarity
+        highest_sim_col = col
+ 
+print("Column %d (book id %s) is most similar to column 0 (book id %s)" %
+        (highest_sim_col, df.columns[col], df.columns[0])
+)
