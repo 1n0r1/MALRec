dff0f439cc3665a6bd2e042a3e178f4aceae6c87
1n0r1 <khanhn2@illinois.edu>
2022-06-10 13:30:22 +0700

    Converted to matrix

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
+chunksize = 10**6
 
-df = pd.read_csv('./data/UserAnimeList.csv')
+fileReader = pd.read_csv('./data/UserAnimeList.csv', chunksize=chunksize)
 
-print(df.iloc[0])
+df = pd.DataFrame(dtype = 'int8')
+
+count = 0
+
+for chunk in fileReader:
+    count +=1
+    if count > 10:
+        break
+    newdf = chunk.pivot(index='username', columns='anime_id', values='my_score').fillna(0).astype('int8')
+    df = pd.concat((df,newdf)).groupby(['username']).first().fillna(0).astype('int8')
+    # df.loc[row.iloc[0], row.iloc[1]] = row.iloc[5]=
+print(df)
